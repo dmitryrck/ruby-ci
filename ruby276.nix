921038ceea22b00cce6ed8784b23072a499518ff
Dmitry Rocha <dmitryrck@gmail.com>
2022-04-13 09:48:51 +1200

    Add ruby276

---
+++ ruby276.nix
@@ -0,0 +1,48 @@
+# build with:
+# ```
+# nix-build ruby276.nix
+# ```
+
+{ pkgs ? import <nixpkgs> {} }:
+with import <nixpkgs> {};
+
+pkgs.stdenv.mkDerivation {
+  name = "ruby276";
+  version = "2.7.6";
+
+  src = pkgs.fetchurl {
+    url = "https://cache.ruby-lang.org/pub/ruby/2.7/ruby-2.7.6.tar.gz";
+    sha256 = "e7203b0cc09442ed2c08936d483f8ac140ec1c72e37bb5c401646b7866cb5d10";
+  };
+
+  phases = [
+    "unpackPhase"
+    "configurePhase"
+    "buildPhase"
+    "installPhase"
+  ];
+
+  buildInputs = [
+    openssl
+    zlib
+    readline
+    gdbm
+  ];
+
+  unpackPhase = ''
+    tar xfz $src
+  '';
+
+  configurePhase = ''
+    cd ruby-$version
+    ./configure --prefix=$out --disable-install-doc --disable-install-rdoc
+  '';
+
+  buildPhase = ''
+    make
+  '';
+
+  installPhase = ''
+    make install
+  '';
+}
